7ce4204a4f6fd1ff8e11aeab41c7f27130e7be6e
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-07-06 09:22:19 -0400

    fix(types): public types had mismatch with published types

--- index.d.ts
+++ index.d.ts
@@ -4,6 +4,6 @@ interface CompileResult {
   name: string;
 }
 export function compileJS(code: string): Promise<CompileResult>;
-export function compileHBS(code: string): CompileResult;
+export function compileHBS(code: string, options?: { scope?: Record<string, unknown> }): CompileResult;
 export function invocationOf(name: string): string;
 export function nameFor(text: string): string;
