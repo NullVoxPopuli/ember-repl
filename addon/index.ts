aa897ada98e45f2256e1e815555903e672ab9c31
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-07-06 09:49:17 -0400

    fix(utils): add missing export

--- addon/index.ts
+++ addon/index.ts
@@ -1,6 +1,6 @@
 export { compileJS } from './cjs';
 export { compileHBS } from './hbs';
-export { invocationOf, nameFor } from './utils';
+export { invocationName, invocationOf, nameFor } from './utils';
 
 // Public Types
 export type { CompileResult } from './types';
